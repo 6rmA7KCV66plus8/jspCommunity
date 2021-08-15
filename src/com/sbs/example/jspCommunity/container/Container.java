bd142537bbebafcc478e9aab4d4e640cc9f74255
6rmA7KCV66plus8 <apnalchangchangkimjeongmin@gmail.com>
2021-08-15 23:40:50 +0900

    META-INF/config.json 을 추가하여, 설정용 서블릿에 읽도록 처리

--- src/com/sbs/example/jspCommunity/container/Container.java
+++ src/com/sbs/example/jspCommunity/container/Container.java
@@ -7,6 +7,7 @@ import com.sbs.example.jspCommunity.controller.UsrMemberController;
 import com.sbs.example.jspCommunity.dao.ArticleDao;
 import com.sbs.example.jspCommunity.dao.MemberDao;
 import com.sbs.example.jspCommunity.service.ArticleService;
+import com.sbs.example.jspCommunity.service.EmailService;
 import com.sbs.example.jspCommunity.service.MemberService;
 
 public class Container {
@@ -19,12 +20,14 @@ public class Container {
 	public static UsrMemberController memberController;
 	public static AdmMemberController admMemberController; // #똑같은 이름의 클래스는 import를 할수 없음
 	public static UsrHomeController homeController;
+	public static EmailService emailService;
 
-	static {
+	static { // 얘가 가장 먼저 실행 됨, 그다음 ConfigServlet의 init부분
 		
 		memberDao = new MemberDao();
 		articleDao = new ArticleDao(); // dao가 더 먼저 만들어져야 함
 		
+		emailService = new EmailService();
 		memberService = new MemberService();
 		articleService = new ArticleService();
 		
